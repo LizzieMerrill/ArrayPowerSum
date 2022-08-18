3e87f1760374a51b566cfbf32dc5197c1e34cb01
LizzieMerrill <97261333+LizzieMerrill@users.noreply.github.com>
2022-08-18 11:59:23 -0700

    Create ArrayPowerSum.java
    Created file and began all 3 questions of the homework.

---
+++ ArrayPowerSum.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class ArrayPowerSum{
+  public static void main(String[] args) {
+    //1.
+    
+    
+    
+    
+    
+    
+    //2. Output is 
+    double[ ] exampleArray = {1,5,6,5,4,1};
+
+          double maximum = examplesArray[0];
+
+          int index = 0;
+
+          for (int i = 1; i<exampleArray.length>; i++){
+
+               if (exampleArray[ i ] > maximum) {
+
+                    maximum = exampleArray[ i ];
+
+                    index = i;
+
+               }
+
+          }
+
+          System.out.println(index);
+    
+    
+    //3.
+    public static int[] toPower(int size, int power){
+      int[] myArray = new Array[size];
+      for (int i = 0; i < myArray.length; ++i){
+      
+      }
+    }
+    
+    
+    
+  }
+}
